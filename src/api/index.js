242d7d472d4cb09a1a2b743e14c47a7c602a7e0a
vinswalker <svinay0488@gmail.com>
2019-10-01 16:38:35 +0530

    security and loader

--- src/api/index.js
+++ src/api/index.js
@@ -7,5 +7,5 @@ const uriConstants = {
 }
 
 export const api_profile_picture = baseURL + "/" + uriConstants.API_REQUEST_PROFILE_PICTURE
-export const api_posts = baseURL + "/" + uriConstants.API_REQUEST_PROFILE_PICTURE
-export const api_friends = baseURL + "/" + uriConstants.API_REQUEST_PROFILE_PICTURE
+export const api_posts = baseURL + "/" + uriConstants.API_REQUEST_POSTS
+export const api_friends = baseURL + "/" + uriConstants.API_REQUEST_FRIENDS
